f0f7dc9928f093dd6acfa8d535d41415973ac177
chavinking <chavinking1990@gmail.com>
2020-10-19 20:08:30 +0800

    commit code

--- tij4-basic-code/src/main/java/concurrency/SimpleThread.java
+++ tij4-basic-code/src/main/java/concurrency/SimpleThread.java
@@ -14,7 +14,7 @@ public class SimpleThread extends Thread {
     }
 
     public String toString() {
-        return "#" + getName() + "(" + countDown + "), ";
+        return "#" + getName() + "(" + countDown + "), \n";
     }
 
     public void run() {
@@ -29,6 +29,7 @@ public class SimpleThread extends Thread {
         for (int i = 0; i < 5; i++)
             new SimpleThread();
     }
+
 } /* Output:
 #1(5), #1(4), #1(3), #1(2), #1(1), #2(5), #2(4), #2(3), #2(2), #2(1), #3(5), #3(4), #3(3), #3(2), #3(1), #4(5), #4(4), #4(3), #4(2), #4(1), #5(5), #5(4), #5(3), #5(2), #5(1),
 *///:~
