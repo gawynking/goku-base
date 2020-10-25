16d9fc6e5c2b30969d05fa4b54417308165096a7
chavinking <chavinking1990@gmail.com>
2020-10-25 14:54:19 +0800

    tij4 ok

--- tij4-basic-code/src/main/java/concurrency/ListComparisons.java
+++ tij4-basic-code/src/main/java/concurrency/ListComparisons.java
@@ -2,76 +2,91 @@
 package concurrency; /* Added by Eclipse.py */
 // {Args: 1 10 10} (Fast verification check during build)
 // Rough comparison of thread-safe List performance.
+
 import java.util.concurrent.*;
 import java.util.*;
+
 import net.mindview.util.*;
 
+
 abstract class ListTest extends Tester<List<Integer>> {
-  ListTest(String testId, int nReaders, int nWriters) {
-    super(testId, nReaders, nWriters);
-  }
-  class Reader extends TestTask {
-    long result = 0;
-    void test() {
-      for(long i = 0; i < testCycles; i++)
-        for(int index = 0; index < containerSize; index++)
-          result += testContainer.get(index);
+
+    ListTest(String testId, int nReaders, int nWriters) {
+        super(testId, nReaders, nWriters);
     }
-    void putResults() {
-      readResult += result;
-      readTime += duration;
+
+    class Reader extends TestTask {
+        long result = 0;
+
+        void test() {
+            for (long i = 0; i < testCycles; i++)
+                for (int index = 0; index < containerSize; index++)
+                    result += testContainer.get(index);
+        }
+
+        void putResults() {
+            readResult += result;
+            readTime += duration;
+        }
     }
-  }
-  class Writer extends TestTask {
-    void test() {
-      for(long i = 0; i < testCycles; i++)
-        for(int index = 0; index < containerSize; index++)
-          testContainer.set(index, writeData[index]);
+
+    class Writer extends TestTask {
+        void test() {
+            for (long i = 0; i < testCycles; i++)
+                for (int index = 0; index < containerSize; index++)
+                    testContainer.set(index, writeData[index]);
+        }
+
+        void putResults() {
+            writeTime += duration;
+        }
     }
-    void putResults() {
-      writeTime += duration;
+
+    void startReadersAndWriters() {
+        for (int i = 0; i < nReaders; i++)
+            exec.execute(new Reader());
+        for (int i = 0; i < nWriters; i++)
+            exec.execute(new Writer());
     }
-  }
-  void startReadersAndWriters() {
-    for(int i = 0; i < nReaders; i++)
-      exec.execute(new Reader());
-    for(int i = 0; i < nWriters; i++)
-      exec.execute(new Writer());
-  }
+
 }
 
+
 class SynchronizedArrayListTest extends ListTest {
-  List<Integer> containerInitializer() {
-    return Collections.synchronizedList(
-      new ArrayList<Integer>(
-        new CountingIntegerList(containerSize)));
-  }
-  SynchronizedArrayListTest(int nReaders, int nWriters) {
-    super("Synched ArrayList", nReaders, nWriters);
-  }
+    List<Integer> containerInitializer() {
+        return Collections.synchronizedList(new ArrayList<Integer>(new CountingIntegerList(containerSize)));
+    }
+
+    SynchronizedArrayListTest(int nReaders, int nWriters) {
+        super("Synched ArrayList", nReaders, nWriters);
+    }
 }
 
 class CopyOnWriteArrayListTest extends ListTest {
-  List<Integer> containerInitializer() {
-    return new CopyOnWriteArrayList<Integer>(
-      new CountingIntegerList(containerSize));
-  }
-  CopyOnWriteArrayListTest(int nReaders, int nWriters) {
-    super("CopyOnWriteArrayList", nReaders, nWriters);
-  }
+    List<Integer> containerInitializer() {
+        return new CopyOnWriteArrayList<Integer>(new CountingIntegerList(containerSize));
+    }
+
+    CopyOnWriteArrayListTest(int nReaders, int nWriters) {
+        super("CopyOnWriteArrayList", nReaders, nWriters);
+    }
 }
 
 public class ListComparisons {
-  public static void main(String[] args) {
-    Tester.initMain(args);
-    new SynchronizedArrayListTest(10, 0);
-    new SynchronizedArrayListTest(9, 1);
-    new SynchronizedArrayListTest(5, 5);
-    new CopyOnWriteArrayListTest(10, 0);
-    new CopyOnWriteArrayListTest(9, 1);
-    new CopyOnWriteArrayListTest(5, 5);
-    Tester.exec.shutdown();
-  }
+    public static void main(String[] args) {
+
+        Tester.initMain(args);
+
+        new SynchronizedArrayListTest(10, 0);
+        new SynchronizedArrayListTest(9, 1);
+        new SynchronizedArrayListTest(5, 5);
+
+        new CopyOnWriteArrayListTest(10, 0);
+        new CopyOnWriteArrayListTest(9, 1);
+        new CopyOnWriteArrayListTest(5, 5);
+
+        Tester.exec.shutdown();
+    }
 } /* Output: (Sample)
 Type                             Read time     Write time
 Synched ArrayList 10r 0w      232158294700              0
--- tij4-basic-code/src/main/java/concurrency/MapComparisons.java
+++ tij4-basic-code/src/main/java/concurrency/MapComparisons.java
@@ -2,82 +2,88 @@
 package concurrency; /* Added by Eclipse.py */
 // {Args: 1 10 10} (Fast verification check during build)
 // Rough comparison of thread-safe Map performance.
+
 import java.util.concurrent.*;
 import java.util.*;
+
 import net.mindview.util.*;
 
-abstract class MapTest
-extends Tester<Map<Integer,Integer>> {
-  MapTest(String testId, int nReaders, int nWriters) {
-    super(testId, nReaders, nWriters);
-  }
-  class Reader extends TestTask {
-    long result = 0;
-    void test() {
-      for(long i = 0; i < testCycles; i++)
-        for(int index = 0; index < containerSize; index++)
-          result += testContainer.get(index);
+abstract class MapTest extends Tester<Map<Integer, Integer>> {
+
+    MapTest(String testId, int nReaders, int nWriters) {
+        super(testId, nReaders, nWriters);
     }
-    void putResults() {
-      readResult += result;
-      readTime += duration;
+
+    class Reader extends TestTask {
+        long result = 0;
+
+        void test() {
+            for (long i = 0; i < testCycles; i++)
+                for (int index = 0; index < containerSize; index++)
+                    result += testContainer.get(index);
+        }
+
+        void putResults() {
+            readResult += result;
+            readTime += duration;
+        }
     }
-  }
-  class Writer extends TestTask {
-    void test() {
-      for(long i = 0; i < testCycles; i++)
-        for(int index = 0; index < containerSize; index++)
-          testContainer.put(index, writeData[index]);
+
+    class Writer extends TestTask {
+        void test() {
+            for (long i = 0; i < testCycles; i++)
+                for (int index = 0; index < containerSize; index++)
+                    testContainer.put(index, writeData[index]);
+        }
+
+        void putResults() {
+            writeTime += duration;
+        }
     }
-    void putResults() {
-      writeTime += duration;
+
+    void startReadersAndWriters() {
+        for (int i = 0; i < nReaders; i++)
+            exec.execute(new Reader());
+        for (int i = 0; i < nWriters; i++)
+            exec.execute(new Writer());
     }
-  }
-  void startReadersAndWriters() {
-    for(int i = 0; i < nReaders; i++)
-      exec.execute(new Reader());
-    for(int i = 0; i < nWriters; i++)
-      exec.execute(new Writer());
-  }
 }
 
 class SynchronizedHashMapTest extends MapTest {
-  Map<Integer,Integer> containerInitializer() {
-    return Collections.synchronizedMap(
-      new HashMap<Integer,Integer>(
-        MapData.map(
-          new CountingGenerator.Integer(),
-          new CountingGenerator.Integer(),
-          containerSize)));
-  }
-  SynchronizedHashMapTest(int nReaders, int nWriters) {
-    super("Synched HashMap", nReaders, nWriters);
-  }
+    Map<Integer, Integer> containerInitializer() {
+        return Collections.synchronizedMap(new HashMap<Integer, Integer>(MapData.map(new CountingGenerator.Integer(),new CountingGenerator.Integer(),containerSize)));
+    }
+
+    SynchronizedHashMapTest(int nReaders, int nWriters) {
+        super("Synched HashMap", nReaders, nWriters);
+    }
 }
 
 class ConcurrentHashMapTest extends MapTest {
-  Map<Integer,Integer> containerInitializer() {
-    return new ConcurrentHashMap<Integer,Integer>(
-      MapData.map(
-        new CountingGenerator.Integer(),
-        new CountingGenerator.Integer(), containerSize));
-  }
-  ConcurrentHashMapTest(int nReaders, int nWriters) {
-    super("ConcurrentHashMap", nReaders, nWriters);
-  }
+    Map<Integer, Integer> containerInitializer() {
+        return new ConcurrentHashMap<Integer, Integer>(MapData.map(new CountingGenerator.Integer(),new CountingGenerator.Integer(), containerSize));
+    }
+
+    ConcurrentHashMapTest(int nReaders, int nWriters) {
+        super("ConcurrentHashMap", nReaders, nWriters);
+    }
 }
 
 public class MapComparisons {
-  public static void main(String[] args) {
-    Tester.initMain(args);
-    new SynchronizedHashMapTest(10, 0);
-    new SynchronizedHashMapTest(9, 1);
-    new SynchronizedHashMapTest(5, 5);
-    new ConcurrentHashMapTest(10, 0);
-    new ConcurrentHashMapTest(9, 1);
-    new ConcurrentHashMapTest(5, 5);
-    Tester.exec.shutdown();
-  }
+    public static void main(String[] args) {
+
+        Tester.initMain(args);
+
+        new SynchronizedHashMapTest(10, 0);
+        new SynchronizedHashMapTest(9, 1);
+        new SynchronizedHashMapTest(5, 5);
+
+        new ConcurrentHashMapTest(10, 0);
+        new ConcurrentHashMapTest(9, 1);
+        new ConcurrentHashMapTest(5, 5);
+
+        Tester.exec.shutdown();
+    }
 } /* Output: (Sample)
 Type                             Read time     Write time
 Synched HashMap 10r 0w        306052025049              0
