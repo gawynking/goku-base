93be3854a6877e8f1786a1abca910403d136cb4e
chavinking <chavinking1990@gmail.com>
2024-03-14 16:36:10 +0800

    remove flink117

--- flink117-basic-code/src/main/java/com/goku/MockData.java
+++
@@ -1,9 +0,0 @@
-package com.goku;
-
-import com.goku.util.MockDataUtils;
-
-public class MockData {
-    public static void main(String[] args) {
-        MockDataUtils.mockOrderStreamData(false, 3000, 3);
-    }
-}
--- flink117-basic-code/src/main/java/com/goku/doc/table/UnimplementedException.java
+++
@@ -1,7 +0,0 @@
-package com.goku.doc.table;
-
-public class UnimplementedException extends RuntimeException {
-    public UnimplementedException() {
-        super("This method has not yet been implemented");
-    }
-}
